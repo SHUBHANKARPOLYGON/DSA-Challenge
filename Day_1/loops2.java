99bb423982c8c783d47d80ef795bdec517a9ac98
SHUBHANKARPOLYGON <shubhankarvishal@gmail.com>
2025-08-11 03:43:40 +0530

    loops2

---
+++ Day_1/loops2.java
@@ -0,0 +1,15 @@
+/*Print numbers 0 to 10 */
+
+package Day_1;
+
+public class loops2 {
+    public static void main(String[] args) {
+        //counter++ => counter=counter+1
+        //counter-- => counter=counter-1
+        //counter+=2 => counter=counter+2
+        //counter-=2 => counter=counter-2
+        for(int counter=0;counter<=10;counter++){
+            System.out.println(counter);
+        }
+    }
+}
