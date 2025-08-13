74d491da1ed4b53b5e2c65bdb184b37d2e2a09f2
SHUBHANKARPOLYGON <shubhankarvishal@gmail.com>
2025-08-13 17:01:18 +0530

    patterns

--- Day_2/patterns.java
+++ Day_2/patterns.java
@@ -2,6 +2,12 @@ package Day_2;
 
 public class patterns {
     public static void main(String[] args) {
-        
+       int n=4;
+       for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            System.out.print(j);
+        }
+        System.out.println(); 
+       } 
     }
 }
