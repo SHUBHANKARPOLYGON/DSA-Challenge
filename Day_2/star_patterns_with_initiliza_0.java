8afef1aa475abbb1abdb61f5240c5dd8243c5f9b
SHUBHANKARPOLYGON <shubhankarvishal@gmail.com>
2025-08-13 20:44:19 +0530

    star pattern 2

---
+++ Day_2/star_patterns_with_initiliza_0.java
@@ -0,0 +1,15 @@
+/* star_patterns_with_initiliza_0 and condition n-1*/
+
+package Day_2;
+
+public class star_patterns_with_initiliza_0 {
+    public static void main(String[] args) {
+        int n=4;
+        for(int i=0;i<=n-1;i++){
+            for(int j=0;j<=n-1;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
