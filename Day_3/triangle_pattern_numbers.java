2862cc266ac6fd6deb16f22db7bcaeb73af44e30
SHUBHANKARPOLYGON <shubhankarvishal@gmail.com>
2025-08-15 23:17:35 +0530

    traingle pattern numbers

---
+++ Day_3/triangle_pattern_numbers.java
@@ -0,0 +1,13 @@
+package Day_3;
+
+public class triangle_pattern_numbers {
+    public static void main(String[] args) {
+        int n=4;
+        for(int i=0;i<=n-1;i++){
+            for(int j=0;j<i+1;j++){
+                System.out.print(i+1+" ");
+            }
+            System.out.println();
+        }
+    }
+}
