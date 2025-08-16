fb0d265dc9720417550ab8109ae0609a0b000980
SHUBHANKARPOLYGON <shubhankarvishal@gmail.com>
2025-08-16 19:24:16 +0530

    reverse

--- Day_3/reverse_triangle_pattern.java
+++ Day_3/reverse_triangle_pattern.java
@@ -5,7 +5,7 @@ public class reverse_triangle_pattern {
         int n=4;
         for(int i=0;i<=n-1;i++){
             for(int j=i+1;j>0;j--){
-                System.out.print(j);
+                System.out.print(j+" ");
             }
             System.out.println();
         }
