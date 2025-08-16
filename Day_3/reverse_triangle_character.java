4e7478bfa0ad68867bd94d6ad048c0b4726df408
SHUBHANKARPOLYGON <shubhankarvishal@gmail.com>
2025-08-17 02:13:02 +0530

    Reverse triangle pattern

--- Day_3/reverse_triangle_character.java
+++ Day_3/reverse_triangle_character.java
@@ -4,7 +4,7 @@ public class reverse_triangle_character {
     public static void main(String[] args) {
         int n=4;
         for(int i=0;i<=n-1;i++){
-            char ch=(char) ('A'+i);
+            char ch=(char) ('A'+i);  // Start character for each row
             for(int j=i+1;j>0;j--){
                 System.out.print(ch+" ");
                 ch--;
