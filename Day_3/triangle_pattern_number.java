6e98245499a58efd0b4f2c3697cc05a1d3d1a61d
SHUBHANKARPOLYGON <shubhankarvishal@gmail.com>
2025-08-16 18:20:58 +0530

    triangle pattern

---
+++ Day_3/triangle_pattern_number.java
@@ -0,0 +1,14 @@
+package Day_3;
+
+public class triangle_pattern_number {
+
+    public static void main(String[] args) {
+        int n = 4;
+        for(int i=0;i<=n-1;i++){
+            for(int j=0;j<i+1;j++){
+                System.out.print(j+1+" "); 
+            }
+            System.out.println();
+        }
+    }
+}
