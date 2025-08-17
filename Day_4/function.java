fff893a8575f555ead486102b8e28ab0d3d8206f
SHUBHANKARPOLYGON <shubhankarvishal@gmail.com>
2025-08-18 02:09:55 +0530

    function 1

---
+++ Day_4/function.java
@@ -0,0 +1,12 @@
+package Day_4;
+
+public class function {
+    
+    void printHello(){
+        System.out.println("Hello from function");
+    }
+    public static void main(String[] args) {
+        function obj=new function();
+        obj.printHello();
+    }
+}
