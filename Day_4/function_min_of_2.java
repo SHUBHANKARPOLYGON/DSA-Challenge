c44ed6162a990739971cc50ae292eaecee857e9e
SHUBHANKARPOLYGON <shubhankarvishal@gmail.com>
2025-08-22 10:48:40 +0530

    function main

--- Day_4/function_min_of_2.java
+++ Day_4/function_min_of_2.java
@@ -9,8 +9,6 @@ public class function_min_of_2 {
             return b;
         }
     }
-    
-    
     public static void main(String[] args) {
         function_min_of_2 obj=new function_min_of_2();
         int result=obj.min(5,10);
