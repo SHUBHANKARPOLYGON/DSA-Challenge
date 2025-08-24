af31c69d8527da91ae5944b05e3d8cea2152001d
SHUBHANKARPOLYGON <shubhankarvishal@gmail.com>
2025-08-24 21:21:57 +0530

    sum of functions done

--- Day_4/sum_in_functions.java
+++ Day_4/sum_in_functions.java
@@ -1,5 +1,19 @@
+//Calculate sum of numbers from 1 to N using functions
+
 package Day_4;
 
 public class sum_in_functions {
-    
+    int sum(int n){
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            sum+=i;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        sum_in_functions obj=new sum_in_functions();
+        int output=obj.sum(5);
+        System.out.println(output);
+    }
 }
