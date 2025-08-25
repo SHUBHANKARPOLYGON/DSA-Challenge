680a9cf5d75e280e269789bd77aa52d2e53414a7
SHUBHANKARPOLYGON <shubhankarvishal@gmail.com>
2025-08-25 20:44:23 +0530

    factorial of number

---
+++ Day_4/N_factorial_fuction.java
@@ -0,0 +1,16 @@
+package Day_4;
+
+public class N_factorial_fuction {
+    int factorial(int n){
+        int num=1;
+        for(int i=1;i<=n;i++){
+            num=num*i;
+        }
+        return num;
+    }
+    public static void main(String[]args){
+        N_factorial_fuction obj=new N_factorial_fuction();
+        int output=obj.factorial(5);
+        System.out.println(output);
+    }
+}
