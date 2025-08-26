1abe2fe86b97518fbc7b0a2547228918f1bef6b5
SHUBHANKARPOLYGON <shubhankarvishal@gmail.com>
2025-08-27 01:29:29 +0530

    pass by value 2

---
+++ Day_4/pass_by_value_2.java
@@ -0,0 +1,17 @@
+package Day_4;
+
+public class pass_by_value_2 {
+    int changex(int x){
+        x=2*x;
+        System.out.println(x);
+
+        return x;
+    }
+    
+    public static void main(String[] args) {
+        int x=5;
+        pass_by_value_2 obj=new pass_by_value_2();
+        obj.changex(x);
+        System.out.println(x);
+    }
+}
