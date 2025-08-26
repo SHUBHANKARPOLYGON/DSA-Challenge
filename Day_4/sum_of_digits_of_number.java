488f5a0fc6d76c7165febe47a4bda4e2ff13138f
SHUBHANKARPOLYGON <shubhankarvishal@gmail.com>
2025-08-27 01:47:02 +0530

    sum of digits

---
+++ Day_4/sum_of_digits_of_number.java
@@ -0,0 +1,18 @@
+package Day_4;
+
+public class sum_of_digits_of_number {
+    int sum(int n){
+        int sum=0;
+        while(n>0){
+            int rem=n%10;
+            sum+=rem; // sum=sum+rem
+            n=n/10;
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        sum_of_digits_of_number obj=new sum_of_digits_of_number();
+        int result=obj.sum(1234);
+        System.out.println(result);
+    }
+}
