711e5be30bd06bdf1cf07a584029c5beb5c80fa0
SHUBHANKARPOLYGON <shubhankarvishal@gmail.com>
2025-09-06 17:21:29 +0530

    binomial coefficient

--- Day_4/binomial_cofficient.java
+++ Day_4/binomial_cofficient.java
@@ -1,7 +1,19 @@
 package Day_4;
 
 public class binomial_cofficient {
-    public static void main(String[] args){
 
+    int binomial(int n){
+        int fact=1;
+        for(int i=1;i<=n;i++){
+            fact*=i;
+        }
+        return fact;
+    }
+    public static void main(String[] args){
+        int n=8;
+        int r=2;
+        binomial_cofficient obj=new binomial_cofficient();
+        int output=obj.binomial(n)/(obj.binomial(n-r)*obj.binomial(r));
+        System.out.println(output);
     }
 }
