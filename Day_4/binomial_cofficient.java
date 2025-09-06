fc2d36126122f6a370f8701d2ecf68d7150de7db
SHUBHANKARPOLYGON <shubhankarvishal@gmail.com>
2025-09-06 18:27:06 +0530

    scanner class

--- Day_4/binomial_cofficient.java
+++ Day_4/binomial_cofficient.java
@@ -1,5 +1,7 @@
 package Day_4;
 
+import java.util.Scanner;
+
 public class binomial_cofficient {
 
     int binomial(int n){
@@ -10,10 +12,15 @@ public class binomial_cofficient {
         return fact;
     }
     public static void main(String[] args){
-        int n=8;
-        int r=2;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter n value:");
+        int n=sc.nextInt();
+        System.out.println("Enter r value:");
+        int r=sc.nextInt();
+        // int n=8;
+        // int r=2;
         binomial_cofficient obj=new binomial_cofficient();
         int output=obj.binomial(n)/(obj.binomial(n-r)*obj.binomial(r));
-        System.out.println(output);
+        System.out.println("Output:"+output);
     }
 }
