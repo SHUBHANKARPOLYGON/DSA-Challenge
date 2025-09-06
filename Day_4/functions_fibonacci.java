21124e7755d00b4a8c123043bb4d12aced49ab56
SHUBHANKARPOLYGON <shubhankarvishal@gmail.com>
2025-09-06 19:17:45 +0530

    fibonacci

---
+++ Day_4/functions_fibonacci.java
@@ -0,0 +1,18 @@
+package Day_4;
+
+public class functions_fibonacci {
+
+    int fibo(int n){
+        if(n==0 || n==1){
+            return n;
+        }else{
+            return fibo(n-1)+fibo(n-2);
+        }
+    }
+    public static void main(String[] args){
+        functions_fibonacci obj=new functions_fibonacci();
+        int result=obj.fibo(10);
+        System.out.println(result);
+    }
+}
+
