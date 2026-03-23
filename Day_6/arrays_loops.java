86c24a5276cac82f922a502ed5ba04982272c2f1
SHUBHANKARPOLYGON <shubhankarvishal@gmail.com>
2026-03-23 19:56:29 +0530

    arrays in the loops

--- Day_6/arrays_loops.java
+++ Day_6/arrays_loops.java
@@ -1,3 +1,25 @@
+
+import java.util.Scanner;
+
 public class arrays_loops {
     
+    public static void main(String[] args) {
+        int size=4;
+        int[] marks=new int[size];     //Array Created with size 4
+        Scanner sc=new Scanner(System.in);    //Scanner class is used to take input from the user
+
+        //Input
+        for(int i=0;i<size;i++){
+            System.out.println("Enter value: "+(i+1));
+            marks[i]=sc.nextInt();    //taking input from the user and storing it in the array at index i
+    }
+    System.out.println();
+
+        //Output
+        for(int i=0;i<size;i++){
+            System.out.println(marks[i]);   //Print the values stored in the array at index i
+        }
+        sc.close();
 }
+}
+   
