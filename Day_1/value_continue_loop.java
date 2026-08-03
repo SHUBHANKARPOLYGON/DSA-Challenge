8667a40a81111852a922f2421c5d25cf25a27e50
SHUBHANKARPOLYGON <shubhankarvishal@gmail.com>
2026-08-03 21:22:00 +0530

    value printing

---
+++ Day_1/value_continue_loop.java
@@ -0,0 +1,10 @@
+package Day_1;
+
+//same value will be printed 5 times because the value of i is not used in the print statement
+public class value_continue_loop {
+    public static void main(String[] args){
+        for(int i=1;i<=5;i++){
+            System.out.println(1);
+        }
+    }
+}
